02b5df0686420519e7c6a4cbb94c073d702ff833
Chen Zixuan <zixuanc360@gmail.com>
2026-02-02 13:45:45 +0800

    Add prac 1 files

---
+++ prac_01/menus.py
@@ -0,0 +1,24 @@
+"""
+CP1404 menus
+"""
+
+name = input("Enter name: ")
+MENU = """(H)ello
+(G)oodbye
+(Q)uit"""
+
+print(MENU)
+choice = input(">>> ").upper()
+
+while choice != "Q":
+    if choice == "H":
+        print(f"Hello {name}")
+    elif choice == "G":
+        print(f"Goodbye {name}")
+    else:
+        print("Invalid choice")
+
+    print(MENU)
+    choice = input(">>> ").upper()
+
+print("Finished.")
